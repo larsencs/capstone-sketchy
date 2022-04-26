36449996fcedeab31f778310b6fb49fb38d3fe53
larsencs <quietus77@gmail.com>
2022-04-26 17:43:26 -0500

    css

--- src/components/posts/AllPost.js
+++ src/components/posts/AllPost.js
@@ -1,8 +1,9 @@
+import "../styles/posts/post.css"
 export const AllPost = ({post}) =>{
 
     return (
-        <div>
-            <div style={{backgroundImage:`url(${post.image})`}}></div>
+        <div className="post_div" style={{backgroundImage:`url(${post.image})`}}>
+            <div ></div>
             <div>
                 <h4>{post.title}</h4>
                 <p>{post.description}</p>
--- src/components/posts/OpenPost.js
+++ src/components/posts/OpenPost.js
@@ -2,6 +2,7 @@ export const OpenPost = ({post}) =>{
     return (
       <>
         <div className="open_prompts" id={post.emotion.emotion}>
+          {/* <div><p>x</p></div> */}
           <h4>{post.prompt.prompt}</h4>
         </div>
       </>
