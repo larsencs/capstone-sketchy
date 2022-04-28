03ea2ebc87ffdf7d7800796de0de8c952c298db0
larsencs <quietus77@gmail.com>
2022-04-28 06:51:15 -0500

    some cloudinary stuff worked on

--- src/components/api/cloudinary.js
+++ src/components/api/cloudinary.js
@@ -1,13 +1,15 @@
 import {Settings} from "../../Settings.js"
 //upload or download after image
-const dataURL = `https://api.cloudinary.com/v1_1/${Settings.cloudName}/image/`
+const dataURL = `${Settings.proxy}https://api.cloudinary.com/v1_1/${Settings.cloudName}/image/`
 
 export const uploadImage = (imageFile) =>{
     return fetch(`${dataURL}/upload/${imageFile}`,{
         method: "POST",
-        headers:{
-            "Access-Control-Allow-Origin"  : "https://www.cloudinary.com"
-        }
+        headers: {
+            "Content-Type" : "application/json",
+            "Access-Control-Allow-Origin" : "*"
+        },
+        body: JSON.stringify(imageFile)
     }).then(res => console.log(res))
 }
 
